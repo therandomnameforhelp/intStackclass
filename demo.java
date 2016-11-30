e328b7694954ba6d62b977f0f0d9231298386998
therandomnameforhelp <keanucorrea@yahoo.com>
2016-11-29 18:32:43 -1000

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,66 @@
+/*
+Name:Final Project
+Developer:Keanu correa
+Date:11/11/2016
+*/
+import java.util.Scanner;
+public class demo {
+   public static void main(String[] args) {
+   
+   int value = 0;
+   int choice = 0;
+   Scanner inTok = new Scanner(System.in);
+   
+   do { 
+   
+   System.out.println("Choose one of these options:");
+   System.out.println("\t1. Add a number to the stack.");
+   System.out.println("\t2. Remove a number from the stack.");
+   System.out.println("\t3. Display the stack.");
+   System.out.println("\t4. Add a number to the queue."); 
+   System.out.println("\t5. Remove a number from the queue.");
+   System.out.println("\t6. Display the queue.");
+   System.out.println("\t7. Quit.");
+   System.out.print("Enter 1, 2, 3, 4, 5, 6, or 7: ");
+   
+   choice = inTok.nextInt();
+        
+
+   switch(choice){
+      case 1:
+      intStack.push(int value);
+      
+      break;
+      
+      case 2:
+      
+      break;
+      
+      case 3:
+      
+      break;
+      
+      case 4:
+      
+      break;
+      
+      case 5:
+      
+      break;
+      
+      case 6:
+      
+      break;
+      
+      case 7:
+      System.out.print("\nGoodbye ");
+      break;
+      
+      default:
+      System.out.println("\n" + choice + " is invalid");
+      }
+      
+   }while (choice != 7);   
+      
+      }
+}
---
+++ intStack.java
@@ -0,0 +1,53 @@
+/*
+Name:Final Project
+Developer:Keanu correa
+Date:11/11/2016
+*/
+
+public class intStack {
+
+   private int[] elements;
+   private int size; 
+   
+   public static final int stackCap = 10;
+   
+   public intStack() {
+      this(stackCap);
+      }
+      
+   public intStack(int capacity) {
+      elements = new int[capacity];
+      }
+
+   public boolean empty() {
+      return size == 0;
+      }
+      
+   public int getSize() {
+      return size;
+      }
+      
+   public int peek() {
+      return elements[size - 1];
+      }
+      
+   public int pop() {
+      return elements[--size];
+      }  
+      
+   public static int push(int value) {
+      return value;
+      }
+   public static void displayStack(String[] args) {
+      intStack stack = new intStack();
+      System.out.println("The Stack of integers is: ");
+      for (int i=0; i<10;i++){
+      stack.push(i);
+      }
+      while (!stack.empty()) {
+      System.out.print(stack.pop() + " "); 
+      }
+   } 
+      
+}
+      
